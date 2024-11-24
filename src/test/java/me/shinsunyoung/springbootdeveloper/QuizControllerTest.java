fedb32bfe3dfdaebb2a95d59ea2fe500fb810878
shangcom <dndbs91@gmail.com>
2024-11-24 17:27:06 +0900

    스프링 부트 3 백엔드 개발자 되기 p119 설명부터

--- src/test/java/me/shinsunyoung/springbootdeveloper/QuizControllerTest.java
+++ src/test/java/me/shinsunyoung/springbootdeveloper/QuizControllerTest.java
@@ -1,2 +1,29 @@
-package me.shinsunyoung.springbootdeveloper;public class QuizControllerTest {
+package me.shinsunyoung.springbootdeveloper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class QuizControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 }
